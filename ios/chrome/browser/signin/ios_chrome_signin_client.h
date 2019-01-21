f54e57d14c185e6f2a6ccf98a1869ad54c6829f2
Mario Sanchez Prada <mario@igalia.com>
2019-01-21 16:50:27 +0100

    Re-added line removed by mistake

--- ios/chrome/browser/signin/ios_chrome_signin_client.h
+++ ios/chrome/browser/signin/ios_chrome_signin_client.h
@@ -1,6 +1,7 @@
 // Copyright 2015 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
+
 #ifndef IOS_CHROME_BROWSER_SIGNIN_IOS_CHROME_SIGNIN_CLIENT_H_
 #define IOS_CHROME_BROWSER_SIGNIN_IOS_CHROME_SIGNIN_CLIENT_H_
 
