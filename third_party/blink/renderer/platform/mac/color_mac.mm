3fc8e7d07a42e9b3195371f7d0a1a19b9730ee2f
Mario Sanchez Prada <mario@igalia.com>
2019-03-08 12:31:28 +0100

    Fix test failures in mac build

--- third_party/blink/renderer/platform/mac/color_mac.mm
+++ third_party/blink/renderer/platform/mac/color_mac.mm
@@ -42,33 +42,33 @@ NSColor* NsColor(const Color& color) {
       // Need this to avoid returning nil because cachedRGBAValues will default
       // to 0.
       DEFINE_STATIC_LOCAL(base::scoped_nsobject<NSColor>, clear_color,
-                          ([NSColor colorWithDeviceRed:0
-                                                 green:0
-                                                  blue:0
-                                                 alpha:0]));
+                          ([[NSColor colorWithDeviceRed:0
+                                                  green:0
+                                                   blue:0
+                                                  alpha:0] retain]));
       return clear_color;
     }
     case Color::kBlack: {
       DEFINE_STATIC_LOCAL(base::scoped_nsobject<NSColor>, black_color,
-                          ([NSColor colorWithDeviceRed:0
-                                                 green:0
-                                                  blue:0
-                                                 alpha:1]));
+                          ([[NSColor colorWithDeviceRed:0
+                                                  green:0
+                                                   blue:0
+                                                  alpha:1] retain]));
       return black_color;
     }
     case Color::kWhite: {
       DEFINE_STATIC_LOCAL(base::scoped_nsobject<NSColor>, white_color,
-                          ([NSColor colorWithDeviceRed:1
-                                                 green:1
-                                                  blue:1
-                                                 alpha:1]));
+                          ([[NSColor colorWithDeviceRed:1
+                                                  green:1
+                                                   blue:1
+                                                  alpha:1] retain]));
       return white_color;
     }
     default: {
       const int kCacheSize = 32;
       static unsigned cached_rgba_values[kCacheSize];
-      static base::scoped_nsobject<NSColor>* cached_colors =
-          new base::scoped_nsobject<NSColor>[kCacheSize];
+      static base::scoped_nsobject<NSColor>* cached_colors(
+          new base::scoped_nsobject<NSColor>[kCacheSize]);
 
       for (int i = 0; i != kCacheSize; ++i) {
         if (cached_rgba_values[i] == c)
